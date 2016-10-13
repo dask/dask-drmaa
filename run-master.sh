c5ec866215461e208e7d3ab0dc431f0bc0ffb376
Matthew Rocklin <mrocklin@gmail.com>
2016-10-13 12:17:10 -0400

    update administrative files around new python library

--- run-master.sh
+++ run-master.sh
@@ -10,13 +10,13 @@ qconf -Msconf /scheduler.txt
 qconf -Ahgrp /hosts.txt
 qconf -Aq /queue.txt
 
-qconf -ah slave_one.daskdrmaa_default
-qconf -ah slave_two.daskdrmaa_default
-qconf -ah slave_three.daskdrmaa_default
+qconf -ah slave_one
+qconf -ah slave_two
+qconf -ah slave_three
 
 qconf -as $HOSTNAME
-bash add_worker.sh dask.q slave_one.daskdrmaa_default 4
-bash add_worker.sh dask.q slave_two.daskdrmaa_default 4
+bash add_worker.sh dask.q slave_one 4
+bash add_worker.sh dask.q slave_two 4
 
 sudo service gridengine-master restart
 

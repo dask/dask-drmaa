fafa85b875cb01371b35c2807a5916849dc886d4
Matthew Rocklin <mrocklin@gmail.com>
2016-10-13 12:16:59 -0400

    add first draft of python library

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from os.path import exists
+from setuptools import setup
+
+setup(name='dask-drmaa',
+      version='0.1.0',
+      description='Dask on DRMAA',
+      url='http://github.com/dask/dask-drmaa/',
+      maintainer='Matthew Rocklin',
+      maintainer_email='mrocklin@gmail.com',
+      license='BSD',
+      keywords='',
+      packages=['dask_drmaa'],
+      install_requires=list(open('requirements.txt').read().strip().split('\n')),
+      long_description=(open('README.rst').read() if exists('README.rst')
+                        else ''),
+      zip_safe=False)

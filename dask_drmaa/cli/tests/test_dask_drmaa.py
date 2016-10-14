296923f8482ba99f6e099851188d54a4f4d93616
Matthew Rocklin <mrocklin@gmail.com>
2016-10-14 08:13:09 -0400

    Use timeout in cli Test

--- dask_drmaa/cli/tests/test_dask_drmaa.py
+++ dask_drmaa/cli/tests/test_dask_drmaa.py
@@ -1,3 +1,5 @@
+from time import sleep, time
+
 from distributed.utils_test import popen
 from distributed import Client
 from distributed.utils_test import loop
@@ -5,4 +7,7 @@ from distributed.utils_test import loop
 def test_dask_drmaa(loop):
     with popen(['dask-drmaa', '2']) as proc:
         with Client('127.0.0.1:8786', loop=loop) as client:
-            assert len(client.ncores()) == 2
+            start = time()
+            while len(client.ncores()) != 2:
+                sleep(0.1)
+                assert time() < start + 30

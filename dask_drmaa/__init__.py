800231750760e439657d66153ad49b1de551d9d4
John Kirkham <kirkhamj@janelia.hhmi.org>
2017-11-05 16:15:20 -0500

    Install versioneer files

--- dask_drmaa/__init__.py
+++ dask_drmaa/__init__.py
@@ -1,3 +1,7 @@
 from .core import DRMAACluster, get_session
 from .sge import SGECluster
 from .adaptive import Adaptive
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
